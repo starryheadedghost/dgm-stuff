c37291e251580e714dbe0010777b505ef06efcbd
Amy Epling <10960924@uvu.edu>
2023-01-19 17:40:19 -0700

    bruh

---
+++ pokemon shelter/Assets/scripts/variables.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class variables : MonoBehaviour
+{
+    //[acces modifier] [data-type/type] [idendifier/name] [access operator =] [value]
+
+    public string NPCname = "sir Nor MalMan";
+
+// Nor MalMan likes bread :)
+    public int NPCbread = 10;
+
+//illegitamite children
+    private float NPCilchildren = 3.4f;
+    
+    public char NPCfavletter = 'm';
+
+    public bool NPCgood = false; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
